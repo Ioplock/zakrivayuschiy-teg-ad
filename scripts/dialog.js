b732873153e238342374c84b4eabf80d350bd8b0
Ioplock <ioplock.me@gmail.com>
2026-05-21 17:35:16 +0300

    fix: prevent dialog button reloads

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,6 +1,16 @@
 const saveButton = document.querySelector(".save-button");
 const memoryDialog = document.querySelector(".memory-dialog");
+const closeDialogButton = memoryDialog.querySelector(".memory-dialog__button");
 
-saveButton.addEventListener("click", () => {
-  memoryDialog.showModal();
+saveButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!memoryDialog.open) {
+    memoryDialog.showModal();
+  }
+});
+
+closeDialogButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  memoryDialog.close();
 });
